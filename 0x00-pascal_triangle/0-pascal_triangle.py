c6201b893e3b7413570d5cd74df9221ee3de0198
Nnennia <nnenniaatuanya@gmail.com>
2023-04-18 13:48:20 +0100

    pushed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,14 @@
-#!/usr/bin/
+#!/usr/bin/python3
 """Pascal's Triangle"""
 def pascal_triangle(n):
+    """Function that prints the pascal's triangle 
+    where n is an integer and n is greater than 0"""
     if n <= 0:
-        return []
+        return [] # return empty list if n is less than or equal to 0
     else:
         pascal_list = []
         for i in range(n):
-            temp_list = []
+            temp_list = [] # a new temporary list to append the contents to the pascal list
             for j in range(i + 1):
                 if j == 0 or j == i:
                     temp_list.append(1)
