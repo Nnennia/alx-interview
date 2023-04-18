4232ba3903e8a7099b65ecd372a5606544391e5a
Nnennia <nnenniaatuanya@gmail.com>
2023-04-18 13:32:40 +0100

    commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/
+"""Pascal's Triangle"""
 def pascal_triangle(n):
     if n <= 0:
         return []
@@ -6,15 +7,11 @@ def pascal_triangle(n):
         pascal_list = []
         for i in range(n):
             temp_list = []
-            for j in range(i+1):
-                if j == 0 or j == 1:
+            for j in range(i + 1):
+                if j == 0 or j == i:
                     temp_list.append(1)
                 else:
-                    number = pascal_list[i -1][j - 1] + pascal_list[i-1][j]
+                    number = pascal_list[i - 1][j - 1] + pascal_list[i - 1][j]
                     temp_list.append(number)
             pascal_list.append(temp_list)
         return pascal_list
-
-
-if __name__ == "__main__":
-    print(pascal_triangle(5))
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,2 @@
+# Pascal's Triangle
+Pascal’s triangle, in algebra, a triangular arrangement of numbers that gives the coefficients in the expansion of any binomial expression, such as (x + y)n. It is named for the 17th-century French mathematician Blaise Pascal, but it is far older.
