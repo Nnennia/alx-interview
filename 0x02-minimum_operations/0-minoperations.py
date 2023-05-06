fcf92e30ba52aa80819934a5ee47c93c8ba14e0e
Nnennia <nnenniaatuanya@gmail.com>
2023-05-06 20:19:42 +0100

    commit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,22 +5,22 @@
 def minOperations(n: int) -> int:
     """Return the minimum number of operations to
     reach 'n' using copy and paste."""
-    pasted = 1
+    if n <= 1:
+        return 0
     count = 0
     copied = 0
-    while n > 1:
+    pasted = 1
+    while pasted < n:
         if copied == 0:
             copied = pasted
             pasted += copied
             count += 2
-        elif (n - pasted) % pasted == 0:
+        elif n - pasted > 0 and (n - pasted) % pasted == 0:
             copied = pasted
             pasted += copied
             count += 2
-        else:
+        elif copied > 0:
+            # paste
             pasted += copied
             count += 1
     return count
-
-
-print(minOperations(9))
