08420e2014453a2c5bdf1eba88eab87a9db9df60
Nnennia <nnenniaatuanya@gmail.com>
2023-05-24 08:45:34 +0100

    comiit

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""A method to determine if a given data set represents a valid UTF-8 encoding"""
-import re
+"""A method to determine if a given data
+set represents a valid UTF-8 encoding"""
 
 
 def validUTF8(data):
     if data == []:
         return True
-    binary_data = (bin(num)[2:].zfill(8) for num in data)
+    binary_data = [bin(num)[2:].zfill(8) for num in data]
     if binary_data[0][0:1] == "0":
         return True
-    elif re.match(r'^110[10]{6}$', binary_data[0]):
-        if len(binary_data) >= 2 and all(re.match(r'^10[01]{7}$', num)
-                                         for num in binary_data[1:2]):
-            return True
-    elif re.match(r'^1110[10]{4}$', binary_data[0]):
-        if len(binary_data) >= 3 and all(re.match(r'^10[01]{7}$', num)
-                                         for num in binary_data[1:3]):
-            return True
-    elif re.match(r'^11110[10]{3}$', binary_data[0]):
-        if len(binary_data) >= 4 and all(re.match(r'^10[01]{7}$', num)
-                                         for num in binary_data[1:4]):
-            return True
-    return False
+    elif binary_data[0][0:3] == "110" \
+            or binary_data[0][0:4] == "1110" and len(binary_data) >= 3 \
+            or binary_data[0][0:5] == "11110" and len(binary_data) >= 4:
+        for bin_num in binary_data[1:]:
+            if bin_num[0:2] != "10":
+                return False
+        return True
+    else:
+        return False
