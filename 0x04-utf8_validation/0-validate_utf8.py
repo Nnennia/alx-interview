de10c75faa4a87a84fa43bf8eec041658636e0d7
Nnennia <nnenniaatuanya@gmail.com>
2023-05-24 08:39:54 +0100

    commit

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,12 +10,15 @@ def validUTF8(data):
     if binary_data[0][0:1] == "0":
         return True
     elif re.match(r'^110[10]{6}$', binary_data[0]):
-        if len(binary_data) >= 2 and all(re.match(r'^10[01]{7}$', num) for num in binary_data[1:2]):
+        if len(binary_data) >= 2 and all(re.match(r'^10[01]{7}$', num)
+                                         for num in binary_data[1:2]):
             return True
     elif re.match(r'^1110[10]{4}$', binary_data[0]):
-        if len(binary_data) >= 3 and all(re.match(r'^10[01]{7}$', num) for num in binary_data[1:3]):
+        if len(binary_data) >= 3 and all(re.match(r'^10[01]{7}$', num)
+                                         for num in binary_data[1:3]):
             return True
     elif re.match(r'^11110[10]{3}$', binary_data[0]):
-        if len(binary_data) >= 4 and all(re.match(r'^10[01]{7}$', num) for num in binary_data[1:4]):
+        if len(binary_data) >= 4 and all(re.match(r'^10[01]{7}$', num)
+                                         for num in binary_data[1:4]):
             return True
     return False
